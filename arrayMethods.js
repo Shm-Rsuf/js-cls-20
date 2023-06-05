6a80b2524ab449a186211fa11fa0067b2a291da5
shm_usuf <shmusuf@gmail.com>
2023-06-05 23:55:06 +0600

    concat, spread operator added

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,7 +1,10 @@
 "use strict";
-const fruits = ["apple", "banana", "orange"];
+/* const fruits = ["apple", "banana", "orange"];
 fruits.push("mango", "papaya");
-
 fruits.unshift("lemon", "coconut");
+console.log(fruits); */
 
-console.log(fruits);
+/* const fruits = ["apple", "banana", "orange"];
+const vesitable = ["carrot", "broccoli"];
+const mixed = [...fruits, ...vesitable];
+console.log(fruits.concat(vesitable)); */
